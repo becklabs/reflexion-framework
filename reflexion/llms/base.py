a3b9faeb6da28337a9589d57a172d8f6d3f587c7
Beck LaBash <becklabash@gmail.com>
2023-08-26 13:42:58 -0700

    ChatML for OpenAICompletionLLM

--- reflexion/llms/base.py
+++ reflexion/llms/base.py
@@ -5,6 +5,9 @@ from langchain.schema import AIMessage, BaseMessage
 
 
 class ChatLLM(ABC):
+    """
+    Abstract base class for a message-thread-based LLM 
+    """
     @abstractmethod
     def __call__(self, messages: List[BaseMessage]) -> AIMessage:
         ...
--- reflexion/llms/openai.py
+++ reflexion/llms/openai.py
@@ -2,31 +2,35 @@ from typing import List
 
 from langchain.chat_models import ChatOpenAI
 from langchain.llms import OpenAI
-from langchain.schema import AIMessage, BaseMessage, HumanMessage
+from langchain.schema import AIMessage, BaseMessage, HumanMessage, SystemMessage
 
 from .base import ChatLLM
 
+import logging
 
 class OpenAIChatLLM(ChatLLM):
-    def __init__(self, *args, **kwargs):
-        self.chat_model = ChatOpenAI(*args, **kwargs)
+    def __init__(self, *model_args, **model_kwargs):
+        self.chat_model = ChatOpenAI(*model_args, **model_kwargs)
 
-    def __call__(self, messages: [List[BaseMessage]]) -> AIMessage:
+    def __call__(self, messages: List[BaseMessage]) -> AIMessage:
         return self.chat_model(messages)
 
-
 class OpenAICompletionLLM(ChatLLM):
+    START_TOKEN = "<|im_start|>"
+    END_TOKEN = "<|im_end|>"
+    MESSAGE_TO_TOKEN = {
+        HumanMessage: "user",
+        AIMessage: "assistant",
+        SystemMessage: "system",
+    }
     def __init__(self, *args, **kwargs):
         self.completion_model = OpenAI(*args, **kwargs)
 
     def __call__(self, messages: List[BaseMessage]) -> AIMessage:
         prompt = ""
         for message in messages:
-            if isinstance(message, AIMessage):
-                prompt += f"ASSISTANT:\n{message.content}\n"
-            elif isinstance(message, HumanMessage):
-                prompt += f"USER:\n{message.content}\n"
-            else:
-                prompt += f"SYSTEM:\n{message.content}\n"
+            prompt += self.START_TOKEN + self.MESSAGE_TO_TOKEN[type(message)] + "\n" + message.content + self.END_TOKEN + "\n"
+        prompt += self.START_TOKEN + self.MESSAGE_TO_TOKEN[AIMessage] + "\n"
+        logging.info(prompt)
 
-        return AIMessage(content=self.completion_model(prompt))
+        return AIMessage(content=self.completion_model(prompt, stop=self.END_TOKEN))
